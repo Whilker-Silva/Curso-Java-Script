980ea3426d1f4d7e5a5b74026f42c1b4982f4721
Whilker Henrique dos Santos Silva <whilker_santos@hotmail.com>
2021-10-22 13:48:29 -0300

    inicio das aulas sobre condições

---
+++ Módulo D/Aula11 - condições parte 1/ex008.js
@@ -0,0 +1,9 @@
+
+var vel = 60;
+console.log(`A velocidade do seu carro é ${vel}`);
+
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade. MULTADO');
+}
+
+console.log(`dirija sempre com sinto de segura!`);
